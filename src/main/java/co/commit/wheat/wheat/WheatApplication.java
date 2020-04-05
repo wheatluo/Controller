f0ad5c7fa5459571fc10cd6b374e41a37f7b3493
unknown <l1218810074@gmail.com>
2020-04-05 18:02:10 +0800

    TiJiao

---
+++ src/main/java/co/commit/wheat/wheat/WheatApplication.java
@@ -0,0 +1,13 @@
+package co.commit.wheat.wheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheatApplication.class, args);
+	}
+
+}
